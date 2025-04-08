edb6d405f56a143fe598c28d579bfabb39f9e407
quefz <a77.00@mail.ru>
2025-04-09 00:12:10 +0500

    Add IndexController

---
+++ educ_plat/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="btn-group-vertical">
+        <a></a>
+    </div>
+@endsection
---
+++ educ_plat/resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>{{ config('app.name') }}</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
